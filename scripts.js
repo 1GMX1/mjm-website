e11c8054c547d3065b00467ac897910f0ca3ebfa
1GMX1 <107010878+1GMX1@users.noreply.github.com>
2022-06-16 07:08:26 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,159 +3,180 @@ window.addEventListener('load', () => {
     const logoLogoSize = document.querySelector('.logo > logo-size');
     const downloadBtn = document.querySelector('.download-btn');
     const downloadBtnTimer = document.querySelector('.download-btn-timer');
-    const downloadBtnProgress = document.querySelector('.download-btn-progress');
+    const downloadBtnProgressTextArea = document.querySelector('.download-btn-progress-text-area');
     var countDownDate = new Date('Jun 30, 2022 23:59:59').getTime();
+    var dateNow = new Date().getTime();
+    var dateDiff = countDownDate - dateNow;
+    var currentDateInSeconds = (dateDiff / 1000);
+    var durationDateInSeconds = (countDownDate / 1000);
     var tweenCountDownDate = null;
     var isTweenCountDownDate = false;
     if (isTweenCountDownDate === true) {
         tweenCountDownDate.pause();
     };
-    tweenCountDownDate = new Between(0, 1)
-    .loop('repeat')
-    .time(1000)
-    .on('start', () => {
-        isTweenCountDownDate = true;
-    })
-    .on('update', (v) => {
-        isTweenCountDownDate = true;
-        if (v >= 1) {
-            var dateNow = new Date().getTime();
-            var dateDiff = countDownDate - dateNow;
-            var currentDateInSeconds = (dateDiff / 1000);
-            var durationDateInSeconds = (countDownDate / 1000);
-            //console.log(dateDiff);
-            curday = Math.floor(((currentDateInSeconds / 60) / 60) / 24);
-            curhour = Math.floor(((currentDateInSeconds / 60) / 60) % 24);
-            curmins = Math.floor((currentDateInSeconds / 60) % 60);
-            cursecs = Math.floor((currentDateInSeconds - (curmins * 60)) % 60);
-            //console.log(Math.floor(((((vdPrListHoverOffsetX) / vdPr.clientWidth) * (video.duration)) - (labelsmins * 60)) % 60));
-            if ((currentDateInSeconds) <= 0) {
-                curday = '';
-                curhour = '';
-                curmins = 0;
-                cursecs = 0;
-                if (cursecs >= 10) cursecs = cursecs; else if (cursecs < 10) cursecs = '0' + cursecs;
-                if (curhour > 0 || curday > 0) {
-                    if (curmins >= 10) {
-                        curmins = curmins;
-                    }else if (curmins < 10) {
-                        curmins = '0' + curmins;
+    if (dateDiff > 0) {
+        tweenCountDownDate = new Between(0, 1)
+        .loop('repeat')
+        .time(1000)
+        .on('start', () => {
+            isTweenCountDownDate = true;
+        })
+        .on('update', (v) => {
+            isTweenCountDownDate = true;
+            if (v >= 1) {
+                dateNow = new Date().getTime();
+                dateDiff = countDownDate - dateNow;
+                currentDateInSeconds = (dateDiff / 1000);
+                durationDateInSeconds = (countDownDate / 1000);
+                //console.log(dateDiff);
+                curday = Math.floor(((currentDateInSeconds / 60) / 60) / 24);
+                curhour = Math.floor(((currentDateInSeconds / 60) / 60) % 24);
+                curmins = Math.floor((currentDateInSeconds / 60) % 60);
+                cursecs = Math.floor((currentDateInSeconds - (curmins * 60)) % 60);
+                //console.log(Math.floor(((((vdPrListHoverOffsetX) / vdPr.clientWidth) * (video.duration)) - (labelsmins * 60)) % 60));
+                if ((currentDateInSeconds) <= 0) {
+                    downloadBtnProgressTextArea.classList.remove('download-btn-progress-text-area-hidden');
+                    downloadBtn.classList.add('hidden');
+                    downloadBtn.addEventListener('mousedown', downloadBtnHidden, false);
+                    curday = '';
+                    curhour = '';
+                    curmins = 0;
+                    cursecs = 0;
+                    if (cursecs >= 10) cursecs = cursecs; else if (cursecs < 10) cursecs = '0' + cursecs;
+                    if (curhour > 0 || curday > 0) {
+                        if (curmins >= 10) {
+                            curmins = curmins;
+                        }else if (curmins < 10) {
+                            curmins = '0' + curmins;
+                        };
+                    }else if (curhour <= 0 || curday <= 0) {
+                        if (curmins >= 10) {
+                            curmins = curmins;
+                        }else if (curmins < 10) {
+                            curmins = curmins;
+                        };
                     };
-                }else if (curhour <= 0 || curday <= 0) {
-                    if (curmins >= 10) {
-                        curmins = curmins;
-                    }else if (curmins < 10) {
-                        curmins = curmins;
+                    if (curhour < 10) {
+                        if (curhour <= 0) {
+                            if (curday <= 0) curhour = ''; else if (curday > 0) curhour = '00:';
+                        }else if (curhour > 0) {
+                            if (curday <= 0) curhour = curhour + ':'; else if (curday > 0) curhour = '0' + curhour + ':';
+                        };
+                    }else if (curhour >= 10) {
+                        curhour = curhour + ':';
                     };
-                };
-                if (curhour < 10) {
-                    if (curhour <= 0) {
-                        if (curday <= 0) curhour = ''; else if (curday > 0) curhour = '00:';
-                    }else if (curhour > 0) {
-                        if (curday <= 0) curhour = curhour + ':'; else if (curday > 0) curhour = '0' + curhour + ':';
-                    };
-                }else if (curhour >= 10) {
-                    curhour = curhour + ':';
-                };
-                if (curday < 10) {
-                    if (curday <= 0) {
-                        curday = '';
-                    }else if (curday > 0) {
+                    if (curday < 10) {
+                        if (curday <= 0) {
+                            curday = '';
+                        }else if (curday > 0) {
+                            curday = curday + ':';
+                        };
+                    }else if (curday >= 10) {
                         curday = curday + ':';
                     };
-                }else if (curday >= 10) {
-                    curday = curday + ':';
-                };
-            }else if ((currentDateInSeconds) >= Math.floor(durationDateInSeconds)) {
-                curday = Math.floor(((durationDateInSeconds / 60) / 60) / 24);
-                curhour = Math.floor(((durationDateInSeconds / 60) / 60) % 24);
-                curmins = Math.floor((durationDateInSeconds / 60) % 60);
-                cursecs = Math.floor((durationDateInSeconds - (curmins * 60)) % 60);
-                if (cursecs >= 10) cursecs = cursecs; else if (cursecs < 10) cursecs = '0' + cursecs;
-                if (curhour > 0 || curday > 0) {
-                    if (curmins >= 10) {
-                        curmins = curmins;
-                    }else if (curmins < 10) {
-                        curmins = '0' + curmins;
+                }else if ((currentDateInSeconds) >= Math.floor(durationDateInSeconds)) {
+                    curday = Math.floor(((durationDateInSeconds / 60) / 60) / 24);
+                    curhour = Math.floor(((durationDateInSeconds / 60) / 60) % 24);
+                    curmins = Math.floor((durationDateInSeconds / 60) % 60);
+                    cursecs = Math.floor((durationDateInSeconds - (curmins * 60)) % 60);
+                    if (cursecs >= 10) cursecs = cursecs; else if (cursecs < 10) cursecs = '0' + cursecs;
+                    if (curhour > 0 || curday > 0) {
+                        if (curmins >= 10) {
+                            curmins = curmins;
+                        }else if (curmins < 10) {
+                            curmins = '0' + curmins;
+                        };
+                    }else if (curhour <= 0 || curday <= 0) {
+                        if (curmins >= 10) {
+                            curmins = curmins;
+                        }else if (curmins < 10) {
+                            curmins = curmins;
+                        };
                     };
-                }else if (curhour <= 0 || curday <= 0) {
-                    if (curmins >= 10) {
-                        curmins = curmins;
-                    }else if (curmins < 10) {
-                        curmins = curmins;
+                    if (curhour < 10) {
+                        if (curhour <= 0) {
+                            if (curday <= 0) curhour = ''; else if (curday > 0) curhour = '00:';
+                        }else if (curhour > 0) {
+                            if (curday <= 0) curhour = curhour + ':'; else if (curday > 0) curhour = '0' + curhour + ':';
+                        };
+                    }else if (curhour >= 10) {
+                        curhour = curhour + ':';
                     };
-                };
-                if (curhour < 10) {
-                    if (curhour <= 0) {
-                        if (curday <= 0) curhour = ''; else if (curday > 0) curhour = '00:';
-                    }else if (curhour > 0) {
-                        if (curday <= 0) curhour = curhour + ':'; else if (curday > 0) curhour = '0' + curhour + ':';
-                    };
-                }else if (curhour >= 10) {
-                    curhour = curhour + ':';
-                };
-                if (curday < 10) {
-                    if (curday <= 0) {
-                        curday = '';
-                    }else if (curday > 0) {
+                    if (curday < 10) {
+                        if (curday <= 0) {
+                            curday = '';
+                        }else if (curday > 0) {
+                            curday = curday + ':';
+                        };
+                    }else if (curday >= 10) {
                         curday = curday + ':';
                     };
-                }else if (curday >= 10) {
-                    curday = curday + ':';
-                };
-            }else {
-                //if (labelshour < 10) labelshour = '0' + labelshour + ':'; else if (labelshour >= 10) labelshour = labelshour + ':';
-                if (cursecs >= 10) cursecs = cursecs; else if (cursecs < 10) cursecs = '0' + cursecs;
-                if (curhour > 0 || curday > 0) {
-                    if (curmins >= 10) {
-                        curmins = curmins;
-                    }else if (curmins < 10) {
-                        curmins = '0' + curmins;
+                }else {
+                    //if (labelshour < 10) labelshour = '0' + labelshour + ':'; else if (labelshour >= 10) labelshour = labelshour + ':';
+                    if (cursecs >= 10) cursecs = cursecs; else if (cursecs < 10) cursecs = '0' + cursecs;
+                    if (curhour > 0 || curday > 0) {
+                        if (curmins >= 10) {
+                            curmins = curmins;
+                        }else if (curmins < 10) {
+                            curmins = '0' + curmins;
+                        };
+                    }else if (curhour <= 0 || curday <= 0) {
+                        if (curmins >= 10) {
+                            curmins = curmins;
+                        }else if (curmins < 10) {
+                            curmins = curmins;
+                        };
                     };
-                }else if (curhour <= 0 || curday <= 0) {
-                    if (curmins >= 10) {
-                        curmins = curmins;
-                    }else if (curmins < 10) {
-                        curmins = curmins;
+                    if (curhour < 10) {
+                        if (curhour <= 0) {
+                            if (curday <= 0) curhour = ''; else if (curday > 0) curhour = '00:';
+                        }else if (curhour > 0) {
+                            if (curday <= 0) curhour = curhour + ':'; else if (curday > 0) curhour = '0' + curhour + ':';
+                        };
+                    }else if (curhour >= 10) {
+                        curhour = curhour + ':';
                     };
-                };
-                if (curhour < 10) {
-                    if (curhour <= 0) {
-                        if (curday <= 0) curhour = ''; else if (curday > 0) curhour = '00:';
-                    }else if (curhour > 0) {
-                        if (curday <= 0) curhour = curhour + ':'; else if (curday > 0) curhour = '0' + curhour + ':';
-                    };
-                }else if (curhour >= 10) {
-                    curhour = curhour + ':';
-                };
-                if (curday < 10) {
-                    if (curday <= 0) {
-                        curday = '';
-                    }else if (curday > 0) {
+                    if (curday < 10) {
+                        if (curday <= 0) {
+                            curday = '';
+                        }else if (curday > 0) {
+                            curday = curday + ':';
+                        };
+                    }else if (curday >= 10) {
                         curday = curday + ':';
                     };
-                }else if (curday >= 10) {
-                    curday = curday + ':';
                 };
+                downloadBtnTimer.innerHTML = curday + curhour + curmins + ':' + cursecs;
             };
-            downloadBtnTimer.innerHTML = curday + curhour + curmins + ':' + cursecs;
-            downloadBtnProgress.style.transform = 'scaleX(' + (((currentDateInSeconds * downloadBtnProgress.clientWidth) / durationDateInSeconds) / downloadBtnProgress.clientWidth) + ')';
-            downloadBtnProgress.style.webkitTransform = 'scaleX(' + (((currentDateInSeconds * downloadBtnProgress.clientWidth) / durationDateInSeconds) / downloadBtnProgress.clientWidth) + ')';
-            downloadBtnProgress.style.MozTransform = 'scaleX(' + (((currentDateInSeconds * downloadBtnProgress.clientWidth) / durationDateInSeconds) / downloadBtnProgress.clientWidth) + ')';
-            downloadBtnProgress.style.msTransform = 'scaleX(' + (((currentDateInSeconds * downloadBtnProgress.clientWidth) / durationDateInSeconds) / downloadBtnProgress.clientWidth) + ')';
-            downloadBtnProgress.style.OTransform = 'scaleX(' + (((currentDateInSeconds * downloadBtnProgress.clientWidth) / durationDateInSeconds) / downloadBtnProgress.clientWidth) + ')';
+            if (dateDiff <= 0) {
+                if (isTweenCountDownDate === true) {
+                    tweenCountDownDate.pause();
+                };
+                downloadBtn.classList.remove('hidden');
+                downloadBtnProgressTextArea.classList.add('download-btn-progress-text-area-hidden');
+                downloadBtn.removeEventListener('mousedown', downloadBtnHidden, false);
+            };
+        })
+        .on('complete', () => {
+            isTweenCountDownDate = false;
+        });
+        downloadBtn.addEventListener('mousedown', downloadBtnHidden, false);
+    }else if (dateDiff <= 0) {
+        if (isTweenCountDownDate === true) {
+            tweenCountDownDate.pause();
         };
-    })
-    .on('complete', () => {
-        isTweenCountDownDate = false;
-    });
+        downloadBtn.classList.remove('hidden');
+        downloadBtnProgressTextArea.classList.add('download-btn-progress-text-area-hidden');
+        downloadBtn.removeEventListener('mousedown', downloadBtnHidden, false);
+    };
+
+    function downloadBtnHidden() {
+        downloadBtn.classList.add('hidden');
+    };
     spanAreaSpan.addEventListener('click', () => {
         window.open('https://' + (window.location.hostname).toString() + '/mjm-website', '_self');
     }, false);
     logoLogoSize.addEventListener('click', () => {
         window.open('https://' + (window.location.hostname).toString() + '/mjm-website', '_self');
-    }, false);
-    downloadBtn.addEventListener('mousedown', () => {
-        downloadBtn.classList.add('hidden');
     }, false);
 }, false);
